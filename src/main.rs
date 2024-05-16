8b5cc9569c95046264e0fb6e40f9567a6e282878
宋秉徽 <451000936@qq.com>
2024-05-17 00:47:24 +0800

    feat: readme

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use inquire::Text;
 
 fn main() {
-    let name = Text::new("What is your name?").prompt();
+    let name = Text::new("请输入要抽样的文件目录").prompt();
 
     match name {
         Ok(name) => println!("Hello {}", name),
         Err(_) => println!("An error happened when asking for your name, try again later."),
     }
 }
+
+// 读文件
+// 文件分类
+// 打印文件分类情况
+// 输入抽样个数
+// 文件抽样
